7647791dfab4adf3ba6b436781a766702ef4bfa7
EC2 Default User <ec2-user@ip-16-0-3-185.ec2.internal>
2026-03-17 14:52:34 +0000

    Fix Dockerfile, add dockerignore and gitignore, update config

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-DB_HOST = os.getenv("DB_HOST", "your-rds-endpoint")
+DB_HOST = os.getenv("DB_HOST", "hospitaldb.cgv8yukoeqoa.us-east-1.rds.amazonaws.com")
 DB_USER = os.getenv("DB_USER", "admin")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "password")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "admin1234")
 DB_NAME = os.getenv("DB_NAME", "hospital")
